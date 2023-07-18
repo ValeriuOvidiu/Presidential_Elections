88dcc68a6283a78685ea4fa09ae8c0b52442df5a
Valeriu Dumitru Ovidiu <valeriuovidiu1999@gmail.com>
2023-07-18 10:59:56 +0300

    commit

--- vote/forms.py
+++ vote/forms.py
@@ -1,21 +1,33 @@
 from django import forms
 
+
 class userForm(forms.Form):
-    first_name=forms.CharField(help_text="Enter your first name" , min_length=1)
-    last_name=forms.CharField(help_text="Enter your last name" )
-    email=forms.EmailField(help_text="Enter your email")
-    password=forms.CharField(help_text="Choose a password",widget=forms.PasswordInput, min_length= 6)
-    confirm_password=forms.CharField(help_text="Confirm the password" ,widget=forms.PasswordInput, min_length=6)    
-       
-    
+    first_name = forms.CharField(help_text="Enter your first name", min_length=1)
+    last_name = forms.CharField(help_text="Enter your last name")
+    email = forms.EmailField(help_text="Enter your email")
+    password = forms.CharField(
+        help_text="Choose a password", widget=forms.PasswordInput, min_length=6
+    )
+    confirm_password = forms.CharField(
+        help_text="Confirm the password", widget=forms.PasswordInput, min_length=6
+    )
+
 
 class codeForm(forms.Form):
-    code=forms.IntegerField(help_text="introduce text", max_value=9999,min_value=1000)
+    code = forms.IntegerField(
+        help_text="introduce text", max_value=9999, min_value=1000
+    )
+
 
 class loginForm(forms.Form):
-    email=forms.EmailField(help_text="Enter your email")
-    password=forms.CharField(help_text="Enter your password",widget=forms.PasswordInput)
+    email = forms.EmailField(help_text="Enter your email")
+    password = forms.CharField(
+        help_text="Enter your password", widget=forms.PasswordInput
+    )
+
 
 class bioForm(forms.Form):
-    bio=forms.CharField(help_text='introduce a small description of yourself',widget=forms.Textarea(attrs={"rows":"6"}))
-      
+    bio = forms.CharField(
+        help_text="introduce a small description of yourself",
+        widget=forms.Textarea(attrs={"rows": "6"}),
+    )
--- vote/models.py
+++ vote/models.py
@@ -3,18 +3,17 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) 
-    bio=models.TextField(default="no bio")       
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default="no bio")
 
     def __str__(self):
-        return f'{self.bio} ' #show how we want it to be displayed 
-    
+        return f"{self.bio} "  # show how we want it to be displayed
+
+
 class candidates(models.Model):
-    candidate=models.OneToOneField(User,on_delete=models.CASCADE)   
+    candidate = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
-    
 class votes_candidate(models.Model):
-    voter= models.OneToOneField(User, on_delete=models.CASCADE )
-    candidate=models.ForeignKey(candidates,on_delete=models.CASCADE)
-   
+    voter = models.OneToOneField(User, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(candidates, on_delete=models.CASCADE)
